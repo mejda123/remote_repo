28d6751c20bc5147a31e4e1db1c8863d3b4f187b
Mejda <MBARAKET@bt0d0000.w2k.bouyguestelecom.fr>
2022-04-05 13:52:43 +0200

    modif 1

--- file.sql
+++ file.sql
@@ -2,3 +2,17 @@
 --test git cas 1
 --test git
 --test git
+--git status: état de l''envirnnement 
+
+
+dossier travail
+|
+GIT add / GIT reset
+|
+V
+dossier index
+|
+git commit -m "premier log message change"
+|
+V
+Dépot local 
